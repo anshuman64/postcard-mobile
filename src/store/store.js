edd9d01477d12366197fc962685c59c20bebc5e8
vinitp94 <vinitp94@gmail.com>
2017-11-16 00:17:22 -0800

    Update thunk export syntax

--- src/store/store.js
+++ src/store/store.js
@@ -2,8 +2,8 @@
 import { createStore, applyMiddleware } from 'redux';
 
 // Local Imports
-import Thunk       from '../middleware/thunk';
-import RootReducer from '../reducers/root_reducer.js';
+import { thunk }   from '../middleware/thunk';
+import RootReducer from '../reducers/root_reducer';
 
 //--------------------------------------------------------------------//
 //--------------------------------------------------------------------//
