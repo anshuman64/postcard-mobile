d1642b07bc846cb3be01cfe3d7ab7100d8ddcc2a
Anshuman Dewangan <anshuman64@gmail.com>
2017-11-21 10:25:53 -0800

    Clean up experiments

--- src/App.js
+++ src/App.js
@@ -1,47 +1,42 @@
 // Library Imports
-import React, { Component }             from 'react';
-import { Platform, StyleSheet, View }   from 'react-native';
-import { StackNavigator, TabNavigator } from 'react-navigation';
+import React                                from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
-import PhoneAuthScreen                  from './components/screens/PhoneAuthScreen.js';
-import ConfirmCodeScreen                from './components/screens/ConfirmCodeScreen.js';
-import AllPostsScreen                   from './components/screens/AllPostsScreen.js';
-import MyPostsScreen                    from './components/screens/MyPostsScreen.js';
-import NewPostScreen                    from './components/screens/NewPostScreen.js';
+//--------------------------------------------------------------------//
 
 
-const RootNavigator = StackNavigator({
-  PhoneAuth: {
-    screen: PhoneAuthScreen
-  },
-  ConfirmCode: {
-    screen: ConfirmCodeScreen
-  },
-  AllPosts: {
-    screen: AllPostsScreen
-  },
-  NewPost: {
-    screen: NewPostScreen
-  }
-});
-
-const PostNavigator = TabNavigator({
-  AllPosts: {
-    screen: RootNavigator
-  },
-  MyPosts: {
-    screen: MyPostsScreen
-  }
-})
-
-
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <PostNavigator />
-    )
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+      </View>
+    );
   }
 }
 
+
+//--------------------------------------------------------------------//
+// Styles
+//--------------------------------------------------------------------//
+
+
+let styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'gray'
+  },
+  welcome: {
+    fontSize: 20,
+    marginTop: 10
+  }
+});
+
+
+//--------------------------------------------------------------------//
 
 export default App;
