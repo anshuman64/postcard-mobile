8b6355d3e7eeb44f980d51ff7e310bf6075a2f83
Anshuman Dewangan <anshuman64@gmail.com>
2017-11-30 22:53:56 -0800

    Set up initial files

--- src/components/new_post_screen/new_post_screen.js
+++ src/components/new_post_screen/new_post_screen.js
@@ -4,16 +4,30 @@ import { Button, StyleSheet, Text, View }   from 'react-native';
 import { connect }                          from 'react-redux';
 
 // Local Imports
+import { styles, scaleFactor }              from './new_post_screen_styles.js';
 import { toBackScreen }                     from '../../actions/navigation_actions.js';
 
 //--------------------------------------------------------------------//
 
 class NewPostScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isBackIconPressed: false,
+    };
+  }
+
   render() {
     return (
-      <View>
-        <Text>NewPost Screen</Text>
-        <Button title={'Go Back'} onPress={() => this.props.navigation.dispatch(toBackScreen())} />
+      <View style={[styles.container]}>
+        {/* Header */}
+
+        {/* Text Input */}
+        <View>
+          <Text>NewPost Screen</Text>
+          <Button title={'Go Back'} onPress={() => this.props.navigation.dispatch(toBackScreen())} />
+        </View>
       </View>
     )
   }
