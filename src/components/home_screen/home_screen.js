c3d69e5829e05b48003d4e31c4db5969abfaf162
Anshuman Dewangan <anshuman64@gmail.com>
2017-12-03 21:44:36 -0800

    Initial post layout

--- src/components/home_screen/home_screen.js
+++ src/components/home_screen/home_screen.js
@@ -1,19 +1,36 @@
 // Library Imports
 import React                                from 'react';
-import { Button, StyleSheet, Text, View }   from 'react-native';
-import { connect }                          from 'react-redux';
+import { Button, StyleSheet, Text, View, FlatList }   from 'react-native';
 
 // Local Imports
+import { styles, scaleFactor }  from './home_screen_styles.js';
+import samplePosts from '../../resources/sample_posts.js';
+import PostListItem from '../post_list_item.js';
 import { toNewPostScreen }                  from '../../actions/navigation_actions.js';
 
 //--------------------------------------------------------------------//
 
+
 class HomeScreen extends React.Component {
+
+  _renderItem = ({item}) => {
+    return (
+      <PostListItem item={item} />
+    )
+  }
+
   render() {
     return (
-      <View>
-        <Text>Home Screen</Text>
-        <Button title={'To NewPost Screen'} onPress={() => this.props.navigation.dispatch(toNewPostScreen())} />
+      <View style={[styles.container]}>
+        <FlatList
+          data={samplePosts}
+          renderItem={this._renderItem}
+          keyExtractor={(item, index) => item.id}
+          style={{width: '100%', height: '100%'}}
+          initialNumToRender={10}
+          maxToRenderPerBatch={10}
+          showsVerticalScrollIndicate={false}
+          />
       </View>
     )
   }
