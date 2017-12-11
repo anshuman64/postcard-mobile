9833e436005d16a1b317d4c10668eed6211d2840
Anshuman Dewangan <anshuman64@gmail.com>
2017-12-10 22:42:18 -0800

    Flatten navigation hierarchy; create separate files

---
+++ src/router/main_tab_navigator.js
@@ -0,0 +1,40 @@
+// Library Imports
+import React                from 'react';
+import { TabNavigator }     from 'react-navigation';
+import Icon                 from 'react-native-vector-icons/SimpleLineIcons';
+
+// Local Imports
+import HomeScreenContainer            from '../components/home_screen/home_screen_container.js';
+import { UserTabNavigator }           from './user_tab_navigator.js';
+import { styles }                     from './main_tab_navigator_styles.js';
+import * as NavigationActionCreators  from '../actions/navigation_actions.js';
+
+//--------------------------------------------------------------------//
+
+export const MainTabNavigator = TabNavigator({
+  HomeScreen: {
+    screen: HomeScreenContainer,
+    navigationOptions:  ({navigation}) => ({
+      tabBarIcon:       ({ focused, tintColor }) => <Icon name='home' style={[styles.icon, focused && styles.iconFocused]} /> ,
+      tabBarOnPress:    (scene) => {if(!scene.focused) {navigation.dispatch(NavigationActionCreators.toHomeScreen())}}
+    })
+  },
+  UserTabNavigator: {
+    screen: UserTabNavigator,
+    navigationOptions:  ({navigation}) => ({
+      tabBarIcon:       ({ focused, tintColor }) => <Icon name='user' style={[styles.icon, focused && styles.iconFocused]} />,
+      tabBarOnPress:    (scene) => {if(!scene.focused) {navigation.dispatch(NavigationActionCreators.toAuthoredPostsTab())}}
+    })
+  },
+}, {
+  tabBarPosition:   'bottom',
+  swipeEnabled:     false,
+  animationEnabled: false,
+  lazy:             true,
+  tabBarOptions: {
+    showLabel:      false,
+    showIcon:       true,
+    style:          styles.style,
+    indicatorStyle: { backgroundColor: 'transparent' },
+  }
+});
