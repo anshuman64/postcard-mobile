3592343b87eb1f89741a0b9af882d0469fab98db
Anshuman Dewangan <anshuman64@gmail.com>
2017-12-12 13:27:45 -0800

    More post API work

--- src/components/user_screen/authored_posts_tab.js
+++ src/components/user_screen/authored_posts_tab.js
@@ -1,7 +1,10 @@
 // Library Imports
 import React  from 'react';
+import RN     from 'react-native';
+import * as _ from 'lodash';
 
 // Local Imports
+import { styles }   from '../home_screen/home_screen_styles.js';
 import PostList     from '../post_list/post_list.js';
 import samplePosts  from '../../test_data/sample_posts.js';
 
@@ -9,10 +12,28 @@ import samplePosts  from '../../test_data/sample_posts.js';
 //--------------------------------------------------------------------//
 
 class AuthoredPostsTab extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authoredPostsData: [],
+    };
+  }
+
+  componentDidMount() {
+    this.props.getAuthoredPosts(this.props.authToken, { limit: 3, offset: this.state.authoredPostsData.length })
+      .then(() => {
+        _.forEach(this.props.authoredPosts, (id) => {
+          this.state.authoredPostsData.push(this.props.postsCache[id])
+        })
+      })
+  }
 
   render() {
     return (
-      <PostList data={samplePosts} />
+      <RN.View style={styles.container} >
+        <PostList data={this.state.authoredPostsData} />
+      </RN.View>
     )
   }
 }
