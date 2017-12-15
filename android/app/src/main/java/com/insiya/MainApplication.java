19f12c0e74c2a42adfb4e45dc6df9d24fc9402e2
Anshuman Dewangan <anshuman64@gmail.com>
2017-12-15 13:41:28 -0800

    Install react-navite-amplitude-analytics; npm update; pod update

--- android/app/src/main/java/com/insiya/MainApplication.java
+++ android/app/src/main/java/com/insiya/MainApplication.java
@@ -3,6 +3,7 @@ package com.insiya;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.sudoplz.reactnativeamplitudeanalytics.RNAmplitudeSDKPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNAmplitudeSDKPackage(),
             new VectorIconsPackage(),
           new RNFirebasePackage(),
           new RNFirebaseAuthPackage()
