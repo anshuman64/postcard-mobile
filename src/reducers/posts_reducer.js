1d9955348436e8a256bb8047f1f431277fcd6c96
Anshuman Dewangan <anshuman64@gmail.com>
2017-12-14 20:26:49 -0800

    Fix bugs for liked posts

--- src/reducers/posts_reducer.js
+++ src/reducers/posts_reducer.js
@@ -2,7 +2,8 @@
 import * as _ from 'lodash';
 
 // Local Imports
-import { POST_ACTION_TYPES, POST_TYPES } from '../actions/post_actions';
+import { POST_ACTION_TYPES, POST_TYPES } from '../actions/post_actions.js';
+import { LIKE_ACTION_TYPES }             from '../actions/like_actions.js';
 
 //--------------------------------------------------------------------//
 
@@ -29,15 +30,23 @@ const PostsReducer = (state = DEFAULT_STATE, action) => {
   let newState = _.merge({}, state);
 
   switch(action.type) {
+
+    //--------------------------------------------------------------------//
+    // Get and Refresh Post Actions
+    //--------------------------------------------------------------------//
+
     case POST_ACTION_TYPES.RECEIVE_POSTS:
       if (action.data.posts.length === 0) {
         switch (action.data.postType) {
           case POST_TYPES.ALL:
             newState.allPosts.isEnd = true;
+            break;
           case POST_TYPES.AUTHORED:
             newState.authoredPosts.isEnd = true;
+            break;
           case POST_TYPES.LIKED:
             newState.likedPosts.isEnd = true;
+            break;
         }
 
         return newState;
@@ -48,14 +57,17 @@ const PostsReducer = (state = DEFAULT_STATE, action) => {
           _.forEach(action.data.posts, (post) => {
             newState.allPosts.data.push(post.id);
           });
+          break;
         case POST_TYPES.AUTHORED:
           _.forEach(action.data.posts, (post) => {
             newState.authoredPosts.data.push(post.id);
           });
+          break;
         case POST_TYPES.LIKED:
           _.forEach(action.data.posts, (post) => {
             newState.likedPosts.data.push(post.id);
           });
+          break;
       }
 
       return newState;
@@ -66,17 +78,25 @@ const PostsReducer = (state = DEFAULT_STATE, action) => {
           newState.allPosts.data = [];
           newState.allPosts.lastUpdated = new Date();
           newState.allPosts.isEnd = false;
+          break;
         case POST_TYPES.AUTHORED:
           newState.authoredPosts.data = [];
           newState.authoredPosts.lastUpdated = new Date();
           newState.authoredPosts.isEnd = false;
+          break;
         case POST_TYPES.LIKED:
           newState.likedPosts.data = [];
           newState.likedPosts.lastUpdated = new Date();
           newState.likedPosts.isEnd = false;
+          break;
       }
 
       return newState;
+
+    //--------------------------------------------------------------------//
+    // Create and Remove Post Actions
+    //--------------------------------------------------------------------//
+
     case POST_ACTION_TYPES.RECEIVE_POST:
       newState.allPosts.data.unshift(action.data.id);
       newState.authoredPosts.data.unshift(action.data.id);
@@ -89,6 +109,21 @@ const PostsReducer = (state = DEFAULT_STATE, action) => {
 
       _.remove(newState.authoredPosts.data, (postId) => {
         return postId === action.data.id;
+      });
+
+      return newState;
+
+    //--------------------------------------------------------------------//
+    // Like Post Actions
+    //--------------------------------------------------------------------//
+
+    case LIKE_ACTION_TYPES.RECEIVE_LIKE:
+      newState.likedPosts.data.unshift(action.data.post_id);
+
+      return newState;
+    case LIKE_ACTION_TYPES.REMOVE_LIKE:
+      _.remove(newState.likedPosts.data, (postId) => {
+        return postId === action.data.post_id;
       });
 
       return newState;
