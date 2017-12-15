fc277cc228cda8097e7e0c8a7fe2a5bd4b745b97
Anshuman Dewangan <anshuman64@gmail.com>
2017-12-15 01:42:44 -0800

    Revert timer

--- src/components/home_screen/home_screen.js
+++ src/components/home_screen/home_screen.js
@@ -11,41 +11,23 @@ import { styles }            from './home_screen_styles.js';
 //--------------------------------------------------------------------//
 
 
-class HomeScreen extends React.Component {
+class HomeScreen extends React.PureComponent {
 
   //--------------------------------------------------------------------//
   // Lifecycle Methods
   //--------------------------------------------------------------------//
 
+  // TODO: fix lastUpdated to work with react navigation
   componentDidMount() {
-    this._refreshPosts();
-    this._startTimer();
-  }
+    if (!this.props.allPosts.lastUpdated) {
+      this.props.refreshPosts(this.props.authToken, POST_TYPES.ALL, {limit: 5})
+      return;
+    }
 
-  componentWillUnmount() {
-    this._stopTimer();
-  }
-
-  //--------------------------------------------------------------------//
-  // Private Methods
-  //--------------------------------------------------------------------//
-
-  _refreshPosts() {
-    this.props.refreshPosts(this.props.authToken, POST_TYPES.ALL);
-  }
-
-  _startTimer() {
-    this.timer = setInterval(this._tick.bind(this), 1000 * 60);
-  }
-
-  _stopTimer() {
-    clearInterval(this.timer);
-  }
-
-  _tick() {
-    this._stopTimer();
-    this._refreshPosts();
-    this._startTimer();
+    let minsDiff = (Date() - this.props.allPosts.lastUpdated) / (1000 * 60)
+    if (minsDiff > 1) {
+      this.props.refreshPosts(this.props.authToken, POST_TYPES.ALL, {limit: 5})
+    }
   }
 
 
