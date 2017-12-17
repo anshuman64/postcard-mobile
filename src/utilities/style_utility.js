9a8b3410a38e91c40594c4f24cec3cd084f2076f
Anshuman Dewangan <anshuman64@gmail.com>
2017-12-17 13:38:26 -0800

    Clean up utilities

--- src/utilities/style_utility.js
+++ src/utilities/style_utility.js
@@ -8,22 +8,22 @@ import { PixelRatio } from 'react-native';
 //--------------------------------------------------------------------//
 
 
+// Apple Color Palette: https://developer.apple.com/ios/human-interface-guidelines/visual-design/color/
+// Google Color Palette: https://material.io/guidelines/style/color.html#color-color-palette
+export const COLORS = {
+  appleRed:  '#ff313a',
+  appleBlue: '#007aff',
+  grey900:   '#212121',
+  grey600:   '#757575',
+  grey400:   '#bdbdbd',
+  grey300:   '#e0e0e0',
+  grey50:    '#fafafa'
+}
+
 export const scale = (size) => {
   return PixelRatio.getPixelSizeForLayoutSize(size)
 }
 
 export const scaleFont = (fontSize) => {
   return fontSize * PixelRatio.getFontScale();
-}
-
-// Apple Color Palette: https://developer.apple.com/ios/human-interface-guidelines/visual-design/color/
-// Google Color Palette: https://material.io/guidelines/style/color.html#color-color-palette
-export const COLORS = {
-  appleRed:   '#ff313a',
-  appleBlue:  '#007aff',
-  grey900:    '#212121',
-  grey600:    '#757575',
-  grey400:    '#bdbdbd',
-  grey300:    '#e0e0e0',
-  grey50:     '#fafafa',
 }
