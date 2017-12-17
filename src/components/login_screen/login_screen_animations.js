c42627227df5627dafe90c77927374f168525d88
Anshuman Dewangan <anshuman64@gmail.com>
2017-12-17 00:49:51 -0800

    Testing animations

--- src/components/login_screen/login_screen_animations.js
+++ src/components/login_screen/login_screen_animations.js
@@ -1,4 +1,28 @@
 
+export const fadeInIcon = {
+  0: {
+    opacity: 0,
+    translateX: 0,
+  },
+  0.5: {
+    opacity: 1,
+    translateX: 0,
+  },
+  1: {
+    opacity: 1,
+    translateX: -50
+  }
+}
+
+export const fadeInLogo = {
+  0: {
+    translateY: 0,
+  },
+  1: {
+    translateY: -200
+  }
+}
+
 export const fadeText = {
   0: {
     opacity: 0.1,
