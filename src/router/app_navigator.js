3f9e474278592a0338b30fe7b449e57547c54a21
Anshuman Dewangan <anshuman64@gmail.com>
2017-12-20 01:18:26 -0800

    Start home screen styling

--- src/router/app_navigator.js
+++ src/router/app_navigator.js
@@ -1,32 +1,33 @@
 // Library Imports
-import React                                                from 'react';
-import RN                                                   from 'react-native';
-import { StackNavigator, TabNavigator, NavigationActions }  from 'react-navigation';
-import Icon                                                 from 'react-native-vector-icons/SimpleLineIcons';
+import React               from 'react';
+import RN                  from 'react-native';
+import { StackNavigator }  from 'react-navigation';
 
 // Local Imports
-import DebugLoginScreenContainer          from '../components/debug_login_screen/debug_login_screen_container.js';
-import LoadingScreenContainer             from '../components/loading_screen/loading_screen_container.js';
-import LoginScreenContainer               from '../components/login_screen/login_screen_container.js';
-import ConfirmCodeScreenContainer         from '../components/confirm_code_screen/confirm_code_screen_container.js';
-import { MainTabNavigator }               from './main_tab_navigator/main_tab_navigator.js';
-import NewPostScreenContainer             from '../components/new_post_screen/new_post_screen_container.js';
-import MenuScreen                         from '../components/menu_screen/menu_screen.js';
-import { toMenuScreen, toNewPostScreen }  from '../actions/navigation_actions.js';
-import { styles }                         from './app_navigator_styles.js';
+import DebugLoginScreenContainer  from '../components/debug_login_screen/debug_login_screen_container.js';
+import LoadingScreenContainer     from '../components/loading_screen/loading_screen_container.js';
+import LoginScreenContainer       from '../components/login_screen/login_screen_container.js';
+import ConfirmCodeScreenContainer from '../components/confirm_code_screen/confirm_code_screen_container.js';
+import { MainTabNavigator }       from './main_tab_navigator/main_tab_navigator.js';
+import NewPostScreenContainer     from '../components/new_post_screen/new_post_screen_container.js';
+import MenuScreen                 from '../components/menu_screen/menu_screen.js';
 
 
 //--------------------------------------------------------------------//
 
 
 export const AppNavigator = StackNavigator({
-  // LoadingScreen: { screen: LoadingScreenContainer },
+  LoadingScreen: { screen: LoadingScreenContainer },
   // DebugLoginScreen: { screen: DebugLoginScreenContainer },
   // LoginScreen: { screen: LoginScreenContainer },
   ConfirmCodeScreen: { screen: ConfirmCodeScreenContainer },
   MainTabNavigator: { screen: MainTabNavigator },
   NewPostScreen: { screen: NewPostScreenContainer },
   MenuScreen: { screen: MenuScreen }
+}, {
+  navigationOptions: {
+    gesturesEnabled: false
+  }
 });
 
 
