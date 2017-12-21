6730037471cc71356f2677f06e2c7837a16afc2c
Anshuman Dewangan <anshuman64@gmail.com>
2017-12-21 00:11:32 -0800

    Start styling menu screen

--- src/components/home_screen/home_screen_styles.js
+++ src/components/home_screen/home_screen_styles.js
@@ -16,33 +16,4 @@ export const styles = RN.StyleSheet.create({
     height: '100%',
     backgroundColor: StyleUtility.COLORS.grey50
   },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '100%',
-    height: 45,
-    paddingLeft: 15,
-    paddingRight: 15,
-    borderBottomColor: StyleUtility.COLORS.grey400,
-    borderBottomWidth: 1,
-    marginTop: StyleUtility.STATUSBAR_HEIGHT,
-    backgroundColor: 'white'
-  },
-  settingsIcon: {
-    fontSize: StyleUtility.scaleFont(16),
-    textAlign: 'center',
-    color: StyleUtility.COLORS.grey900
-  },
-  logo: {
-    height: '70%',
-  },
-  noteIcon: {
-    fontSize: StyleUtility.scaleFont(16),
-    textAlign: 'center',
-    color: StyleUtility.COLORS.appleBlue,
-  },
-  iconHighlighted: {
-    color: StyleUtility.COLORS.appleBlue
-  },
 });
