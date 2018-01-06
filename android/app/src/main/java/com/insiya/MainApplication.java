e15505d79011bfda893ff65efb42549f4f16e147
Anshuman Dewangan <anshuman64@gmail.com>
2018-01-05 20:07:32 -0800

    npm install react-native-fetch-blob

--- android/app/src/main/java/com/insiya/MainApplication.java
+++ android/app/src/main/java/com/insiya/MainApplication.java
@@ -3,6 +3,7 @@ package com.insiya;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.sudoplz.reactnativeamplitudeanalytics.RNAmplitudeSDKPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFetchBlobPackage(),
             new RNAmplitudeSDKPackage(MainApplication.this),
             new VectorIconsPackage(),
           new RNFirebasePackage(),
