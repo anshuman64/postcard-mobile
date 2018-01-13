d074c9e4db0192749ea2124addfa7e07426c2d75
Anshuman Dewangan <anshuman64@gmail.com>
2018-01-13 15:41:16 -0800

    Implement randomized placeholder texts

---
+++ src/utilities/new_post_utility.js
@@ -0,0 +1,11 @@
+//--------------------------------------------------------------------//
+// Interface
+//--------------------------------------------------------------------//
+
+export const POST_PLACEHOLDERS = [
+  'Suggestion: How are you?',
+  'Suggestion: What is your greatest fear?',
+  'Suggestion: What was your happiest moment?',
+  'Suggestion: What was your happiest moment today?',
+
+]
