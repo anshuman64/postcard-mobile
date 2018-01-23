2e725351124957345c903e4ac30064994cdc9a8e
Vinit Parikh <vinitp94@gmail.com>
2018-01-22 23:11:03 -0800

    Clean up function_utility

--- src/utilities/function_utility.js
+++ src/utilities/function_utility.js
@@ -2,26 +2,25 @@
 // Interface
 //--------------------------------------------------------------------//
 
+
 // Checks if string is empty (null, blank, or spaces only)
 export const isStringEmpty = (string) => {
-  return (string.length === 0 || !string.trim());
-}
+  return string.length === 0 || !string.trim();
+};
 
 // Gets a random integer between 0 and max
 export const getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max));
-}
+};
 
 // Callback function to set state on current component
 export const setStateCallback = (component, state) => {
-  let func = () => {
+  return () => {
     component.setState(state);
-  }
+  };
+};
 
-  return func;
-}
-
-export const renderLikesCount = (count) => {
+export const getReadableCount = (count) => {
   // If likes < 1000, render the number as-is
   if (count < 1000) {
     return count;
@@ -44,21 +43,21 @@ export const mergeSorted = (arrayA, arrayB) => {
 
   while (i < m && j < n){
     if (arrayA[i] > arrayB[j]){
-      arrayC.push(arrayA[i])
-      i += 1
+      arrayC.push(arrayA[i]);
+      i += 1;
     } else if (arrayB[j] > arrayA[i]) {
-      arrayC.push(arrayB[j])
-      j+= 1
+      arrayC.push(arrayB[j]);
+      j+= 1;
     } else {
-      arrayC.push(arrayB[j])
-      j += 1
-      i += 1
+      arrayC.push(arrayB[j]);
+      j += 1;
+      i += 1;
     }
   }
 
   while (j < n) {
-    arrayC.push(arrayB[j])
-    j += 1
+    arrayC.push(arrayB[j]);
+    j += 1;
   }
 
   return arrayC;
