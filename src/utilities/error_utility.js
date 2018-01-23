cc08f12b8270e71d267015ca35f6852ca7d183d6
Vinit Parikh <vinitp94@gmail.com>
2018-01-22 23:39:05 -0800

    Clean error utility

--- src/utilities/error_utility.js
+++ src/utilities/error_utility.js
@@ -1,7 +1,6 @@
 // Library Imports
 import { Alert } from 'react-native';
 
-
 //--------------------------------------------------------------------//
 
 
@@ -12,13 +11,13 @@ import { Alert } from 'react-native';
 // Pops appropriate alert depending on error
 export function defaultErrorAlert(error) {
   if (error.description === 'No internet connection') {
-    Alert.alert('','No internet connection.',[{text: 'OK', style: 'cancel'}]);
+    Alert.alert('', 'No internet connection.', [{ text: 'OK', style: 'cancel' }]);
   } else {
-    Alert.alert('','Something went wrong. Please try again later.',[{text: 'OK', style: 'cancel'}]);
+    Alert.alert('', 'Something went wrong. Please try again later.', [{ text: 'OK', style: 'cancel' }]);
   }
 
-  console.error(error)
-}
+  console.error(error);
+};
 
 // Checks if error has a description. If not, add the description listed and return the error
 export function setErrorDescription(error, description) {
@@ -27,4 +26,4 @@ export function setErrorDescription(error, description) {
   }
 
   return error;
-}
+};
