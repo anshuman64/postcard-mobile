392b864c3646387e0c9776be9564570348a13b35
Anshuman Dewangan <anshuman64@gmail.com>
2018-01-27 10:31:37 -0800

    Ensure keyboard is dismissed when changing screens

--- src/actions/navigation_actions.js
+++ src/actions/navigation_actions.js
@@ -2,39 +2,66 @@ import { Keyboard } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
 //--------------------------------------------------------------------//
+// Helper Functions
+//--------------------------------------------------------------------//
+
+// Ensures keyboard is dismissed before navigating to next screen
+let dismissKeyBoard = () => {
+  return new Promise((resolve, reject) => {
+    Keyboard.dismiss();
+    setTimeout(() => resolve(), 1);
+  });
+};
+
+//--------------------------------------------------------------------//
 // Action Creators
 //--------------------------------------------------------------------//
 
 // Navigates to particular screen and sends props. Dismisses keyboard before navigating.
 export const navigateTo = (screen, props) => (dispatch) => {
-  Keyboard.dismiss();
+  let navigateScreens = () => {
+    if (screen === 'DebugLoginScreen') {
+      Actions.DebugLoginScreen(props)
+    } else if (screen === 'LoadingScreen') {
+      Actions.LoadingScreen(props)
+    } else if (screen === 'WelcomeScreen') {
+      Actions.WelcomeScreen(props)
+    } else if (screen === 'LoginScreen') {
+      Actions.LoginScreen(props)
+    } else if (screen === 'ConfirmCodeScreen') {
+      Actions.ConfirmCodeScreen(props)
+    } else if (screen === 'UsernameScreenLogin') {
+      Actions.UsernameScreenLogin(props)
+    } else if (screen === 'AvatarScreen') {
+      Actions.AvatarScreen(props)
+    } else if (screen === 'HomeScreen') {
+      Actions.HomeScreen(props)
+    } else if (screen === 'ProfileScreen') {
+      Actions.ProfileScreen(props)
+    } else if (screen === 'NewPostScreen') {
+      Actions.NewPostScreen(props)
+    } else if (screen === 'MenuScreen') {
+      Actions.MenuScreen(props)
+    } else if (screen === 'UsernameScreen') {
+      Actions.UsernameScreen(props)
+    } else if (screen === 'CameraRollScreen') {
+      Actions.CameraRollScreen(props)
+    } else {
+      return;
+    }
+  }
 
-  if (screen === 'DebugLoginScreen') {
-    Actions.DebugLoginScreen(props)
-  } else if (screen === 'LoadingScreen') {
-    Actions.LoadingScreen(props)
-  } else if (screen === 'WelcomeScreen') {
-    Actions.WelcomeScreen(props)
-  } else if (screen === 'LoginScreen') {
-    Actions.LoginScreen(props)
-  } else if (screen === 'ConfirmCodeScreen') {
-    Actions.ConfirmCodeScreen(props)
-  } else if (screen === 'UsernameScreenLogin') {
-    Actions.UsernameScreenLogin(props)
-  } else if (screen === 'AvatarScreen') {
-    Actions.AvatarScreen(props)
-  } else if (screen === 'HomeScreen') {
-    Actions.HomeScreen(props)
-  } else if (screen === 'ProfileScreen') {
-    Actions.ProfileScreen(props)
-  } else if (screen === 'NewPostScreen') {
-    Actions.NewPostScreen(props)
-  } else if (screen === 'MenuScreen') {
-    Actions.MenuScreen(props)
-  } else if (screen === 'UsernameScreen') {
-    Actions.UsernameScreen(props)
-  } else if (screen === 'CameraRollScreen') {
-    Actions.CameraRollScreen(props)
+  if (Actions.currentScene === 'LoginScreen'
+      || Actions.currentScene === 'ConfirmCodeScreen'
+      || Actions.currentScene === 'UsernameScreenLogin'
+      || Actions.currentScene === 'UsernameScreen'
+      || Actions.currentScene === 'NewPostScreen') {
+    dismissKeyBoard()
+      .then(() => {
+        navigateScreens();
+      })
+  } else {
+    navigateScreens();
   }
 }
 
