5dbb16314859b171f4b286b127495c9663dad680
Anshuman Dewangan <anshuman64@gmail.com>
2018-01-28 11:39:24 -0800

    Paginate camera roll

--- src/components/post_list/post_list_styles.js
+++ src/components/post_list/post_list_styles.js
@@ -9,7 +9,7 @@ import * as StyleUtility         from '../../utilities/style_utility.js';
 
 //--------------------------------------------------------------------//
 
-let footerTextWidth = StyleUtility.scaleFont(120);
+const FOOTER_TEXT_WIDTH = StyleUtility.scaleFont(120);
 
 export const styles = StyleSheet.create({
   postList: {
@@ -32,7 +32,7 @@ export const styles = StyleSheet.create({
     height: 50,
   },
   footerText: {
-    width: footerTextWidth,
+    width: FOOTER_TEXT_WIDTH,
     fontFamily: StyleUtility.setAndroidFont('Roboto-Light'),
     fontWeight: '100',
     fontSize: 14,
@@ -41,7 +41,7 @@ export const styles = StyleSheet.create({
   },
   horizontalLine: {
     alignSelf: 'flex-start',
-    width: (StyleUtility.getUsableDimensions().width - footerTextWidth) / 2 - 20, // Device width minus footerText width over 2 minus aesthetic value
+    width: (StyleUtility.getUsableDimensions().width - FOOTER_TEXT_WIDTH) / 2 - 20, // Device width minus footerText width over 2 minus aesthetic value
     height: '50%',
     borderBottomWidth: 1,
     borderBottomColor: StyleUtility.COLORS.grey200
