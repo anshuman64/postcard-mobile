c4e8dad018ba7971ebfa814f783787584ca13180
Anshuman Dewangan <anshuman64@gmail.com>
2018-01-30 09:39:32 -0800

    Set up reducer; set up actions file; rename file utility functions

--- src/reducers/root_reducer.js
+++ src/reducers/root_reducer.js
@@ -5,6 +5,7 @@ import { combineReducers } from 'redux';
 import UserReducer       from './user_reducer.js'
 import PostsReducer      from './posts_reducer.js';
 import PostsCacheReducer from './posts_cache_reducer.js';
+import ImageReducer      from './image_reducer.js';
 import NavigationReducer from './navigation_reducer.js';
 
 //--------------------------------------------------------------------//
