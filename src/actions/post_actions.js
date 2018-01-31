1ace6521bbd16128576cb6d6e8507375181db1da
Anshuman Dewangan <anshuman64@gmail.com>
2018-01-30 22:21:20 -0800

    Finish getImage refactor

--- src/actions/post_actions.js
+++ src/actions/post_actions.js
@@ -126,7 +126,14 @@ export const createPost = (authToken, firebaseUserObj, userId, postBody, postIma
     .then((newPost) => {
       amplitude.logEvent('Engagement - Create Post', { is_successful: true, body: postBody, image: postImage ? true : false, placeholder_text: placeholderText });
       dispatch(receivePost({ post: newPost, userId: userId }));
-      dispatch(getImage(firebaseUserObj, newPost.author_avatar_url));
+
+      if (newPost.image_url) {
+        dispatch(getImage(firebaseUserObj, newPost.image_url));
+      }
+
+      if (newPost.author_avatar_url) {
+        dispatch(getImage(firebaseUserObj, newPost.author_avatar_url));
+      }
     })
     .catch((error) => {
       if (error.message === "Invalid access token. 'Expiration time' (exp) must be in the future.") {
