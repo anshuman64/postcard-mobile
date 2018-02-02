9b5510d92506965e35e972ff228474af084c7907
Anshuman Dewangan <anshuman64@gmail.com>
2018-02-02 11:05:30 -0800

    Fix small errors

--- src/screens/avatar_screen/avatar_screen_container.js
+++ src/screens/avatar_screen/avatar_screen_container.js
@@ -6,6 +6,7 @@ import AvatarScreen                     from './avatar_screen.js';
 import { editAvatar, refreshAuthToken } from '../../actions/user_actions.js';
 import { refreshCredsAndGetImage }      from '../../actions/image_actions.js';
 import { navigateTo, goBack }           from '../../actions/navigation_actions.js';
+import { uploadFile }                   from '../../utilities/file_utility.js';
 
 
 //--------------------------------------------------------------------//
@@ -21,6 +22,7 @@ const mapStateToProps = ({ user, images }, ownProps) => ({
 const mapDispatchToProps = (dispatch, ownProps) => ({
   editAvatar:              (authToken, firebaseUserObj, avatarUrl) => dispatch(editAvatar(authToken, firebaseUserObj, avatarUrl)),
   refreshCredsAndGetImage: (firebaseUserObj, avatarUrl) => dispatch(refreshCredsAndGetImage(firebaseUserObj, avatarUrl)),
+  uploadFile:              (authToken, firebaseUserObj, imagePath, imageType, userId, folderPath) => dispatch(uploadFile(authToken, firebaseUserObj, imagePath, imageType, userId, folderPath)),
   navigateTo:              (screen, props) => dispatch(navigateTo(screen, props)),
   goBack:                  (props) => dispatch(goBack(props)),
   refreshAuthToken:        (firebaseUserObj, func, ...params) => dispatch(refreshAuthToken(firebaseUserObj, func, ...params)),
