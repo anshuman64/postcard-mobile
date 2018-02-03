cf2374571a60d2a37ca339ee701ef7e04aa8d88a
Anshuman Dewangan <anshuman64@gmail.com>
2018-02-02 20:07:53 -0800

    Implement share list item

---
+++ src/components/share_list_item/share_list_item_container.js
@@ -0,0 +1,24 @@
+// Library Imports
+import { connect } from 'react-redux';
+
+// Local Imports
+import ShareListItem               from './header.js';
+import { refreshCredsAndGetImage } from '../../../actions/image_actions.js';
+
+
+//--------------------------------------------------------------------//
+
+
+const mapStateToProps = ({ user, images }, ownProps) => ({
+  firebaseUserObj: user.firebaseUserObj,
+  images: images
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  refreshCredsAndGetImage: (firebaseUserObj, avatarUrl) => dispatch(refreshCredsAndGetImage(firebaseUserObj, avatarUrl)),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ShareListItem);
