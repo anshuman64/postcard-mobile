80025a22aa8a5775babfabac21bddf43a7c15c00
Anshuman Dewangan <anshuman64@gmail.com>
2018-02-05 13:46:16 -0800

    Get all posts on loading screen

--- src/screens/confirm_code_screen/confirm_code_screen_container.js
+++ src/screens/confirm_code_screen/confirm_code_screen_container.js
@@ -2,22 +2,26 @@
 import { connect } from 'react-redux';
 
 // Local Imports
-import ConfirmCodeScreen                                           from './confirm_code_screen.js';
-import { getConfirmationCode, verifyConfirmationCode, loginClient }  from '../../actions/client_actions.js';
-import { navigateTo }                                              from '../../actions/navigation_actions.js';
+import ConfirmCodeScreen                                            from './confirm_code_screen.js';
+import { getConfirmationCode, verifyConfirmationCode, loginClient } from '../../actions/client_actions.js';
+import { getPosts }                                                 from '../../actions/post_actions.js';
+import { navigateTo }                                               from '../../actions/navigation_actions.js';
 
 
 //--------------------------------------------------------------------//
 
 
 const mapStateToProps = ({ client }, ownProps) => ({
-  client: client.user,
+  authToken:       client.authToken,
+  firebaseUserObj: client.firebaseUserObj,
+  client:          client.user,
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   getConfirmationCode:    (phoneNumber) => dispatch(getConfirmationCode(phoneNumber)),
   verifyConfirmationCode: (confirmationCodeObj, inputtedCode) => dispatch(verifyConfirmationCode(confirmationCodeObj, inputtedCode)),
-  loginClient:              (firebaseUserObj) => dispatch(loginClient(firebaseUserObj)),
+  loginClient:            (firebaseUserObj) => dispatch(loginClient(firebaseUserObj)),
+  getPosts:               (authToken, firebaseUserObj, isRefresh, userId, postType, isUser, queryParams) => dispatch(getPosts(authToken, firebaseUserObj, isRefresh, userId, postType, isUser, queryParams)),
   navigateTo:             (screen, props) => dispatch(navigateTo(screen, props))
 });
 
