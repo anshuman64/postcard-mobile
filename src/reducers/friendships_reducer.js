91d05d07e6bd6c175410342235c18c28e292e24b
Anshuman Dewangan <anshuman64@gmail.com>
2018-02-05 10:24:58 -0800

    Implement createFriendship action

--- src/reducers/friendships_reducer.js
+++ src/reducers/friendships_reducer.js
@@ -7,15 +7,19 @@ import { FRIENDSHIP_ACTION_TYPES }  from '../actions/friendship_actions.js';
 //--------------------------------------------------------------------//
 
 
-const DEFAULT_STATE = {};
+const DEFAULT_STATE = {
+  friends:          [],
+  sentRequests:     [],
+  receivedRequests: [],
+};
 
 const FriendshipsReducer = (state = DEFAULT_STATE, action) => {
   Object.freeze(state);
   let newState = _.merge({}, state);
 
   switch(action.type) {
-    case FRIENDSHIP_ACTION_TYPES.GET:
-      // newState.currentScreen = Actions.currentScene.replace(/^_/, '');
+    case FRIENDSHIP_ACTION_TYPES.RECEIVE_FRIEND_REQUEST:
+      newState.sentRequests.unshift(action.data.user);
 
       return newState;
     default:
