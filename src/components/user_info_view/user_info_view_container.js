91e97324029f2963cf45a6be304a3523c46ccf1a
Anshuman Dewangan <anshuman64@gmail.com>
2018-02-06 20:10:30 -0800

    Refactor user info view

--- src/components/user_info_view/user_info_view_container.js
+++ src/components/user_info_view/user_info_view_container.js
@@ -2,7 +2,8 @@
 import { connect } from 'react-redux';
 
 // Local Imports
-import UserInfoView from './user_info_view.js';
+import UserInfoView          from './user_info_view.js';
+import { navigateToProfile } from '../../actions/navigation_actions.js';
 
 
 //--------------------------------------------------------------------//
@@ -14,6 +15,11 @@ const mapStateToProps = ({ client, usersCache, imagesCache }, ownProps) => ({
   imagesCache: imagesCache
 });
 
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  navigateToProfile: (props) => dispatch(navigateToProfile(props))
+});
+
 export default connect(
   mapStateToProps,
+  mapDispatchToProps
 )(UserInfoView);
