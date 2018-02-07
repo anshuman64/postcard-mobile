fe3b1e31759b6694ecd0af1539b2a2b1bf0c0861
Anshuman Dewangan <anshuman64@gmail.com>
2018-02-06 21:17:44 -0800

    Avatar screen fixes

--- src/screens/avatar_screen/avatar_screen_container.js
+++ src/screens/avatar_screen/avatar_screen_container.js
@@ -2,11 +2,10 @@
 import { connect } from 'react-redux';
 
 // Local Imports
-import AvatarScreen                     from './avatar_screen.js';
-import { editAvatar, refreshAuthToken } from '../../actions/client_actions.js';
-import { refreshCredsAndGetImage }      from '../../actions/image_actions.js';
-import { navigateTo, goBack }           from '../../actions/navigation_actions.js';
-import { uploadFile }                   from '../../utilities/file_utility.js';
+import AvatarScreen                 from './avatar_screen.js';
+import { editAvatar }               from '../../actions/client_actions.js';
+import { refreshCredsAndGetImage }  from '../../actions/image_actions.js';
+import { navigateTo, goBack }       from '../../actions/navigation_actions.js';
 
 
 //--------------------------------------------------------------------//
@@ -21,10 +20,8 @@ const mapStateToProps = ({ client, usersCache, imagesCache }, ownProps) => ({
 const mapDispatchToProps = (dispatch, ownProps) => ({
   editAvatar:              (authToken, firebaseUserObj, userId, imagePath, imageType) => dispatch(editAvatar(authToken, firebaseUserObj, userId, imagePath, imageType)),
   refreshCredsAndGetImage: (firebaseUserObj, avatarUrl) => dispatch(refreshCredsAndGetImage(firebaseUserObj, avatarUrl)),
-  uploadFile:              (authToken, firebaseUserObj, imagePath, imageType, userId, folderPath) => dispatch(uploadFile(authToken, firebaseUserObj, imagePath, imageType, userId, folderPath)),
   navigateTo:              (screen, props) => dispatch(navigateTo(screen, props)),
   goBack:                  (props) => dispatch(goBack(props)),
-  refreshAuthToken:        (firebaseUserObj, func, ...params) => dispatch(refreshAuthToken(firebaseUserObj, func, ...params)),
 });
 
 export default connect(
