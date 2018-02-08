4921ea045809144e215cba981453fbd2618b4615
Anshuman Dewangan <anshuman64@gmail.com>
2018-02-08 13:04:16 -0800

    Set up Pusher

--- src/utilities/analytics_utility.js
+++ src/utilities/analytics_utility.js
@@ -2,7 +2,7 @@
 import RNAmplitute from 'react-native-amplitude-analytics';
 
 // Local Imports
-import { ENV_TYPES, ANALYTICS_ENV_SETTING } from '../app_config.js';
+import { ENV_TYPES, AMPLITUDE_ENV_SETTING } from '../app_config.js';
 
 //--------------------------------------------------------------------//
 
@@ -12,13 +12,17 @@ import { ENV_TYPES, ANALYTICS_ENV_SETTING } from '../app_config.js';
 
 // Chooses the right API key depending on environment setting
 let setupAmplitude = () => {
-  if (ANALYTICS_ENV_SETTING === ENV_TYPES.PRODUCTION) {
-    return new RNAmplitute('7ce84b314a6daad5ff9966ec1a2c52ab'); // key for insiya-production
-  } else if (ANALYTICS_ENV_SETTING === ENV_TYPES.TEST) {
-    return new RNAmplitute('754c90a8ee32f23bd7042c47ea600e4d'); // key for insiya-test
+  let apiKey;
+
+  if (AMPLITUDE_ENV_SETTING === ENV_TYPES.PRODUCTION) {
+    apiKey = '7ce84b314a6daad5ff9966ec1a2c52ab'; // key for insiya-production
+  } else if (AMPLITUDE_ENV_SETTING === ENV_TYPES.TEST) {
+    apiKey = '754c90a8ee32f23bd7042c47ea600e4d'; // key for insiya-test
   } else {
-    return new RNAmplitute('fa9aded0e5b7590482fffff78b2bd85c'); // key for insiya-dev
+    apiKey = 'fa9aded0e5b7590482fffff78b2bd85c'; // key for insiya-dev
   }
+
+  return new RNAmplitute(apiKey)
 };
 
 //--------------------------------------------------------------------//
