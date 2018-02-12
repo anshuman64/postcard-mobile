2d1e9e3ae522b2f69cf2dc9d5c934af5bab118d6
Anshuman Dewangan <anshuman64@gmail.com>
2018-02-11 19:14:50 -0800

    Implement blocking

--- src/screens/debug_login_screen/debug_login_screen_container.js
+++ src/screens/debug_login_screen/debug_login_screen_container.js
@@ -2,10 +2,11 @@
 import { connect } from 'react-redux';
 
 // Local Imports
-import DebugLoginScreen   from './debug_login_screen.js';
-import { debugSignIn }    from '../../actions/client_actions.js';
-import { getFriendships } from '../../actions/friendship_actions.js';
-import { navigateTo }     from '../../actions/navigation_actions.js';
+import DebugLoginScreen    from './debug_login_screen.js';
+import { debugSignIn }     from '../../actions/client_actions.js';
+import { getFriendships }  from '../../actions/friendship_actions.js';
+import { getBlockedUsers } from '../../actions/block_actions.js';
+import { navigateTo }      from '../../actions/navigation_actions.js';
 
 //--------------------------------------------------------------------//
 
@@ -15,9 +16,10 @@ const mapStateToProps = ({ client, usersCache }, ownProps) => ({
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  debugSignIn:    (email, password) => dispatch(debugSignIn(email, password)),
-  getFriendships: (authToken, firebaseUserObj, friendType) => dispatch(getFriendships(authToken, firebaseUserObj, friendType)),
-  navigateTo:     (screen, props) => dispatch(navigateTo(screen, props))
+  debugSignIn:     (email, password) => dispatch(debugSignIn(email, password)),
+  getFriendships:  (authToken, firebaseUserObj, friendType) => dispatch(getFriendships(authToken, firebaseUserObj, friendType)),
+  getBlockedUsers: (authToken, firebaseUserObj) => dispatch(getBlockedUsers(authToken, firebaseUserObj)),
+  navigateTo:      (screen, props) => dispatch(navigateTo(screen, props))
 });
 
 export default connect(
