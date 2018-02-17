5ff3ca977d5f2d4469219383b144669aafe54314
Anshuman Dewangan <anshuman64@gmail.com>
2018-02-16 16:23:31 -0800

    Implement routing on push notification open

--- src/screens/loading_screen/loading_screen_container.js
+++ src/screens/loading_screen/loading_screen_container.js
@@ -2,11 +2,11 @@
 import { connect } from 'react-redux';
 
 // Local Imports
-import LoadingScreen      from './loading_screen';
-import { loginClient }    from '../../actions/client_actions';
-import { getFriendships } from '../../actions/friendship_actions';
-import { getBlockedUsers } from '../../actions/block_actions';
-import { navigateTo }     from '../../actions/navigation_actions';
+import LoadingScreen                      from './loading_screen';
+import { loginClient }                    from '../../actions/client_actions';
+import { getFriendships }                 from '../../actions/friendship_actions';
+import { getBlockedUsers }                from '../../actions/block_actions';
+import { navigateTo, navigateToMessages } from '../../actions/navigation_actions';
 
 //--------------------------------------------------------------------//
 
@@ -16,10 +16,11 @@ const mapStateToProps = ({ client, usersCache }, ownProps) => ({
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  navigateTo:     (screen, props) => dispatch(navigateTo(screen, props)),
-  loginClient:    (firebaseUserObj) => dispatch(loginClient(firebaseUserObj)),
-  getFriendships: (authToken, firebaseUserObj, friendType) => dispatch(getFriendships(authToken, firebaseUserObj, friendType)),
-  getBlockedUsers: (authToken, firebaseUserObj) => dispatch(getBlockedUsers(authToken, firebaseUserObj)),
+  navigateTo:         (screen, props) => dispatch(navigateTo(screen, props)),
+  loginClient:        (firebaseUserObj) => dispatch(loginClient(firebaseUserObj)),
+  getFriendships:     (authToken, firebaseUserObj, friendType) => dispatch(getFriendships(authToken, firebaseUserObj, friendType)),
+  getBlockedUsers:    (authToken, firebaseUserObj) => dispatch(getBlockedUsers(authToken, firebaseUserObj)),
+  navigateToMessages: (props) => dispatch(navigateToMessages(props)),
 });
 
 export default connect(
