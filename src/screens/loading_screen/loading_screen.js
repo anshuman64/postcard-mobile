c36c43eceb51ac2d29858fcad87567ecb575f39f
Anshuman Dewangan <anshuman64@gmail.com>
2018-04-16 13:45:52 -0700

    Consolidate loading into loading screen

--- src/screens/loading_screen/loading_screen.js
+++ src/screens/loading_screen/loading_screen.js
@@ -24,8 +24,7 @@ class LoadingScreen extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    this.isLoggedIn      = false;
-    this.isOver          = false;
+    this.isAnimationEnd  = false;
     this.currentAppState = 'active';
     this.lastUpdate      = new Date();
   }
@@ -51,15 +50,7 @@ class LoadingScreen extends React.PureComponent {
             if (this.props.client.is_banned) {
               RN.Alert.alert('', 'This account has been disabled. Email support@insiya.io for more info.', [{text: 'OK', style: 'cancel'}]);
             } else {
-              this._getPosts();
-              this._loadAllData()
-                .then(() => {
-                  this.isLoggedIn = true;
-                  this._onAnimationEnd();
-                })
-                .catch((error) => {
-                  defaultErrorAlert(error);
-                });
+              this._onLogin();
             }
           })
           .catch((error) => {
@@ -75,42 +66,6 @@ class LoadingScreen extends React.PureComponent {
   componentWillUnmount() {
     RN.AppState.removeEventListener('change', this._handleAppStateChange);
     OneSignal.removeEventListener('opened', this._onOpened);
-  }
-
-  //--------------------------------------------------------------------//
-  // Private Methods
-  //--------------------------------------------------------------------//
-
-  _getPosts = () => {
-    for (let postType in POST_TYPES) {
-      this.props.getPosts(this.props.client.authToken, this.props.client.firebaseUserObj, true, this.props.client.id, POST_TYPES[postType], true);
-    }
-  }
-
-  async _loadAllData() {
-    await this.props.getBlockedUsers(this.props.client.authToken, this.props.client.firebaseUserObj);
-
-    await this._loadData();
-  }
-
-  async _loadData()  {
-    for (let friendType in FRIEND_TYPES) {
-      await this.props.getFriendships(this.props.client.authToken, this.props.client.firebaseUserObj, FRIEND_TYPES[friendType]);
-    }
-  }
-
-  _onOpened = (openResult) => {
-    let data = openResult.notification.payload.additionalData;
-
-    if (data.type === 'receive-like') {
-      this.props.navigateTo('ProfileScreen');
-    } else if (data.type === 'receive-friendship' || data.type === 'receive-accepted-friendship') {
-      this.props.navigateTo('FriendScreen', { tab: true });
-    } else if (data.type === 'receive-post') {
-      this.props.navigateTo('HomeScreen');
-    } else if (data.type === 'receive-message') {
-      this.props.navigateTo('MessagesScreen', { userId: data.client.id });
-    }
   }
 
   //--------------------------------------------------------------------//
@@ -137,28 +92,52 @@ class LoadingScreen extends React.PureComponent {
     this.currentAppState = nextAppState;
   }
 
-  _onAnimationEnd = () => {
+  _onOpened = (openResult) => {
+    let data = openResult.notification.payload.additionalData;
+
+    if (data.type === 'receive-like') {
+      this.props.navigateTo('ProfileScreen');
+    } else if (data.type === 'receive-friendship' || data.type === 'receive-accepted-friendship') {
+      this.props.navigateTo('FriendScreen', { tab: true });
+    } else if (data.type === 'receive-post') {
+      this.props.navigateTo('HomeScreen');
+    } else if (data.type === 'receive-message') {
+      this.props.navigateTo('MessagesScreen', { userId: data.client.id });
+    }
+  }
+
+  _onLogin = () => {
     if (this.unsubscribe) {
       this.unsubscribe();
     }
 
-    if (this.isOver) {
+    this.isAnimationEnd = true;
+    let client = this.props.usersCache[this.props.client.id];
+
+    for (let postType in POST_TYPES) {
+      this.props.getPosts(this.props.client.authToken, this.props.client.firebaseUserObj, true, this.props.client.id, POST_TYPES[postType], true);
+    }
+
+    this.props.getBlockedUsers(this.props.client.authToken, this.props.client.firebaseUserObj);
+
+    for (let friendType in FRIEND_TYPES) {
+      this.props.getFriendships(this.props.client.authToken, this.props.client.firebaseUserObj, FRIEND_TYPES[friendType]);
+    }
+
+    if (client && client.username) {
+      this.props.navigateTo('HomeScreen');
+    } else {
+      this.props.navigateTo('UsernameScreenLogin');
+    }
+  }
+
+  _onAnimationEnd = () => {
+    if (this.isAnimationEnd) {
       return;
     }
 
-    this.isOver = true;
-
-    if (this.isLoggedIn) {
-      let client = this.props.usersCache[this.props.client.id];
-
-      if (client && client.username) {
-        return this.props.navigateTo('HomeScreen');
-      } else {
-        return this.props.navigateTo('UsernameScreenLogin');
-      }
-    } else {
-      return this.props.navigateTo('WelcomeScreen');
-    }
+    this.isAnimationEnd = true;
+    this.props.navigateTo('WelcomeScreen');
   }
 
   //--------------------------------------------------------------------//
