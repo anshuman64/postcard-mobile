85cb648374a69788599f63b04a1154b84048d8b5
anshuman64 <anshuman64@users.noreply.github.com>
2018-04-16 06:04:17 -0700

    Revert "Update node modules & pods"

--- src/app_config.js
+++ src/app_config.js
@@ -14,8 +14,8 @@ export const SERVER_ENV_SETTING    = ENV_TYPES.PRODUCTION
 // WARNING -- affects the AWS S3 bucket
 export const AWS_ENV_SETTING       = ENV_TYPES.PRODUCTION
 
-// WARNING -- affects the Pusher project
+// WARNING -- affects the Amplitude Analytics project
 export const PUSHER_ENV_SETTING    = ENV_TYPES.PRODUCTION
 
 // WARNING -- affects the Amplitude Analytics project
-export const AMPLITUDE_ENV_SETTING = ENV_TYPES.DEV
+export const AMPLITUDE_ENV_SETTING = ENV_TYPES.PRODUCTION
