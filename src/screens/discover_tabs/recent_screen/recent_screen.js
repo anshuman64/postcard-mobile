71ca5b3eadb5ea1432ff7b21c5f161cd48832c91
Anshuman Dewangan <anshuman64@gmail.com>
2018-04-18 07:54:13 -0700

    Remove auto-refresh after 2 mins in discover tab

--- src/screens/discover_tabs/recent_screen/recent_screen.js
+++ src/screens/discover_tabs/recent_screen/recent_screen.js
@@ -12,31 +12,6 @@ import { UTILITY_STYLES } from '../../../utilities/style_utility';
 class RecentScreen extends React.PureComponent {
 
   //--------------------------------------------------------------------//
-  // Lifecycle Methods
-  //--------------------------------------------------------------------//
-
-  // Auto-refresh screen if coming back to it after > 2 minutes
-  componentWillReceiveProps(nextProps) {
-    if (this.props.currentScreen != 'RecentScreen' && nextProps.currentScreen === 'RecentScreen') {
-      this._checkRefresh(POST_TYPES.PUBLIC);
-    }
-  }
-
-  //--------------------------------------------------------------------//
-  // Private Methods
-  //--------------------------------------------------------------------//
-
-  _checkRefresh = (postType) => {
-    let currentTime = new Date();
-    let lastUpdate = this.props.posts[this.props.client.id][postType].lastUpdated;
-    let minsDiff = (currentTime - lastUpdate) / (1000 * 60);
-
-    if (minsDiff > 2) {
-      this.postList.getWrappedInstance()._onRefresh(postType);
-    }
-  }
-
-  //--------------------------------------------------------------------//
   // Render Methods
   //--------------------------------------------------------------------//
 
