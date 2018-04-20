c4bc576e44731c81d05fa39eb5803a054c89628f
Anshuman Dewangan <anshuman64@gmail.com>
2018-04-19 17:23:33 -0700

    Set up group actions

---
+++ src/actions/group_actions.js
@@ -0,0 +1,97 @@
+// Local Imports
+import { amplitude }           from '../utilities/analytics_utility';
+import * as APIUtility         from '../utilities/api_utility';
+import { setErrorDescription } from '../utilities/error_utility';
+import { refreshAuthToken }    from './client_actions';
+
+//--------------------------------------------------------------------//
+
+//--------------------------------------------------------------------//
+// Constants
+//--------------------------------------------------------------------//
+
+export const GROUP_ACTION_TYPES = {
+  RECEIVE_GROUPS: 'RECEIVE_GROUPS',
+  RECEIVE_GROUP:  'RECEIVE_GROUP',
+  REMOVE_GROUP:   'REMOVE_GROUP',
+};
+
+//--------------------------------------------------------------------//
+// Action Creators
+//--------------------------------------------------------------------//
+
+// groups (array): array of group objects
+export const receiveGroups = (data) => {
+  return { type: GROUP_ACTION_TYPES.RECEIVE_GROUPS, data: data };
+};
+
+// group (group object): group object of created group
+export const receiveGroup = (data) => {
+  return { type: GROUP_ACTION_TYPES.RECEIVE_GROUP, data: data };
+};
+
+// group (group object): group object of destroyed group
+export const removeGroup = (data) => {
+  return { type: GROUP_ACTION_TYPES.REMOVE_GROUP, data: data };
+};
+
+//--------------------------------------------------------------------//
+// Asynchronous Actions
+//--------------------------------------------------------------------//
+
+// Gets all groups for client
+export const getGroups = (authToken, firebaseUserObj) => (dispatch) => {
+  return APIUtility.get(authToken, '/groups')
+    .then((groups) => {
+      dispatch(receiveGroups({ groups: groups }));
+    })
+    .catch((error) => {
+      if (error.message === "Invalid access token. 'Expiration time' (exp) must be in the future.") {
+        return dispatch(refreshAuthToken(firebaseUserObj, getGroups));
+      }
+
+      throw setErrorDescription(error, 'GET groups failed');
+    });
+};
+
+// Creates group with list of user_ids
+// TODO: revise amplitude names
+export const createGroup = (authToken, firebaseUserObj, users) => (dispatch) => {
+  return APIUtility.post(authToken, '/groups', { user_ids: users })
+    .then((newGroup) => {
+      amplitude.logEvent('Groups - Create Group', { is_successful: true, num_users: users.length });
+      // dispatch(receiveGroup({ group: newGroup }));
+    })
+    .catch((error) => {
+      if (error.message === "Invalid access token. 'Expiration time' (exp) must be in the future.") {
+        return dispatch(refreshAuthToken(firebaseUserObj, createGroup, name, users));
+      }
+
+      if (error.message === 'Minimum 2 user_ids required') {
+        error = setErrorDescription(error, 'Minimum 2 user_ids required');
+      } else {
+        error = setErrorDescription(error, 'POST groups failed');
+      }
+
+      amplitude.logEvent('Groups - Create Group', { is_successful: false, num_users: users.length, error_description: error.description, error_message: error.message });
+      throw error;
+    });
+};
+
+// Deletes group
+export const deleteGroup = (authToken, firebaseUserObj, groupId) => (dispatch) => {
+  return APIUtility.del(authToken, '/groups/' + groupId)
+    .then((deletedGroup) => {
+      amplitude.logEvent('Groups - Delete Group', { is_successful: true });
+      dispatch(removeGroup({ group: deletedGroup }));
+    })
+    .catch((error) => {
+      if (error.message === "Invalid access token. 'Expiration time' (exp) must be in the future.") {
+        return dispatch(refreshAuthToken(firebaseUserObj, deleteGroup, groupId));
+      }
+
+      error = setErrorDescription(error, 'DEL group failed');
+      amplitude.logEvent('Groups - Delete Group', { is_successful: false, error_description: error.description, error_message: error.message });
+      throw error;
+    });
+};
