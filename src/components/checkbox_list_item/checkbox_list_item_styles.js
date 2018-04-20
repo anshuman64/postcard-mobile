3e322e71892c7d160b8869caac33f1b3cbb9a3db
Anshuman Dewangan <anshuman64@gmail.com>
2018-04-20 16:01:55 -0700

    Implement remove member from group

--- src/components/checkbox_list_item/checkbox_list_item_styles.js
+++ src/components/checkbox_list_item/checkbox_list_item_styles.js
@@ -10,17 +10,6 @@ import * as StyleUtility from '../../utilities/style_utility';
 const DEFAULT_MARGIN = 10;
 
 export const styles = StyleSheet.create({
-  rowView: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: StyleUtility.getUsableDimensions().width - 2 * DEFAULT_MARGIN,
-    height: 60,
-    marginLeft: DEFAULT_MARGIN,
-    marginRight: DEFAULT_MARGIN,
-    borderBottomWidth: 1,
-    borderBottomColor: StyleUtility.COLORS.grey200
-  },
   userView: {
     flexDirection: 'row',
     justifyContent: 'flex-start',
