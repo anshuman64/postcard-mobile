1ec01188fde4156211ca61eee4ffc51c27f2eded
Anshuman Dewangan <anshuman64@gmail.com>
2018-04-20 07:26:16 -0700

    Add CreateGroupScreen to Header

--- src/components/header/header_container.js
+++ src/components/header/header_container.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Header                  from './header';
 import { createPost }          from '../../actions/post_actions';
 import { createCircle }        from '../../actions/circle_actions';
+import { createGroup }         from '../../actions/group_actions';
 import { navigateTo, goBack }  from '../../actions/navigation_actions';
 
 //--------------------------------------------------------------------//
@@ -16,6 +17,7 @@ const mapStateToProps = ({ client }, ownProps) => ({
 const mapDispatchToProps = (dispatch, ownProps) => ({
   createPost:   (authToken, firebaseUserObj, clientId, isPublic, recipients, postBody, postImagePath, postImageType, placeholderText) => dispatch(createPost(authToken, firebaseUserObj, clientId, isPublic, recipients, postBody, postImagePath, postImageType, placeholderText)),
   createCircle: (authToken, firebaseUserObj, name, users) => dispatch(createCircle(authToken, firebaseUserObj, name, users)),
+  createGroup:  (authToken, firebaseUserObj, users) => dispatch(createGroup(authToken, firebaseUserObj, users)),
   navigateTo:   (screen, props) => dispatch(navigateTo(screen, props)),
   goBack:       (props) => dispatch(goBack(props)),
 });
