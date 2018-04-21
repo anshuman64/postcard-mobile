a1cd55e43998e6ef49b753535077a39811e7cbb7
Anshuman Dewangan <anshuman64@gmail.com>
2018-04-21 09:50:30 -0700

    Remove unnecessary style files

--- src/screens/friend_tabs/friend_screen/friend_screen.js
+++ src/screens/friend_tabs/friend_screen/friend_screen.js
@@ -8,7 +8,6 @@ import ListHeader                    from '../../../components/list_header/list_
 import ConversationListItemContainer from '../../../components/conversation_list_item/conversation_list_item_container';
 import ListFooter                    from '../../../components/list_footer/list_footer';
 import SectionListHeader             from '../../../components/section_list_header/section_list_header';
-import { styles }                    from './friend_screen_styles';
 import { UTILITY_STYLES, scaleFont } from '../../../utilities/style_utility';
 
 //--------------------------------------------------------------------//
