0c411ae1479960631317636f4da8174c5d978354
Anshuman Dewangan <anshuman64@gmail.com>
2018-04-22 10:19:08 -0700

    Make keyExtractor into strings to stop warning

--- src/screens/friend_tabs/friend_screen/friend_screen.js
+++ src/screens/friend_tabs/friend_screen/friend_screen.js
@@ -64,7 +64,7 @@ class FriendScreen extends React.PureComponent {
     return (
       <RN.SectionList
         sections={[{data: this.props.conversations, renderItem: this._renderItem.bind(this), title: 'Conversations'}]}
-        keyExtractor={(item) => item}
+        keyExtractor={(item, index) => 'list-item-${index}'}
         renderSectionHeader={this._renderSectionHeader.bind(this)}
         ListHeaderComponent={this._renderHeader()}
         ListFooterComponent={this._renderFooter()}
