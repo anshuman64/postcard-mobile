6391e28ee18ccdad6006959844a334a824d07c13
Anshuman Dewangan <anshuman64@gmail.com>
2018-04-22 17:01:27 -0700

    Fix index as string bug

--- src/screens/friend_tabs/pending_screen/pending_screen.js
+++ src/screens/friend_tabs/pending_screen/pending_screen.js
@@ -84,7 +84,7 @@ class PendingScreen extends React.PureComponent {
           {data: this.props.friendships.contacts, renderItem: this._renderItem.bind(this), title: 'Contacts on Postcard'},
           {data: this.props.blocks.blockedUsers, renderItem: this._renderItem.bind(this), title: 'Blocked Users'},
         ]}
-        keyExtractor={(item, index) => 'list-item-${index}'}
+        keyExtractor={(item, index) => String(index)}
         renderSectionHeader={this._renderSectionHeader.bind(this)}
         ListHeaderComponent={this._renderHeader()}
         initialListSize={20}
