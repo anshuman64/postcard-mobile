c3d91256e59d5297e41a5013f7d4ae69d585d9ce
Anshuman Dewangan <anshuman64@gmail.com>
2018-04-23 12:21:28 -0700

    Clean up reducer imports

--- src/reducers/friendships_reducer.js
+++ src/reducers/friendships_reducer.js
@@ -2,8 +2,8 @@
 import _ from 'lodash';
 
 // Local Imports
-import { FRIEND_TYPES, FRIENDSHIP_ACTION_TYPES } from '../actions/friendship_actions';
-import { MESSAGE_ACTION_TYPES }                  from '../actions/message_actions';
+import { FRIENDSHIP_ACTION_TYPES } from '../actions/friendship_actions';
+import { MESSAGE_ACTION_TYPES }    from '../actions/message_actions';
 
 //--------------------------------------------------------------------//
 
