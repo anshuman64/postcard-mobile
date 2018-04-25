db1c0e75dea677f398da9294ff1c2fb52b2bcd13
Anshuman Dewangan <anshuman64@gmail.com>
2018-04-25 11:02:21 -0700

    Update production settings

--- src/app_config.js
+++ src/app_config.js
@@ -18,4 +18,4 @@ export const AWS_ENV_SETTING       = ENV_TYPES.PRODUCTION
 export const PUSHER_ENV_SETTING    = ENV_TYPES.PRODUCTION
 
 // WARNING -- affects the Amplitude Analytics project
-export const AMPLITUDE_ENV_SETTING = ENV_TYPES.DEV
+export const AMPLITUDE_ENV_SETTING = ENV_TYPES.PRODUCTION
