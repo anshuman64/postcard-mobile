38c0a819be183daeb6a1a211f2974d985ae5d8a0
Anshuman Dewangan <anshuman64@gmail.com>
2018-04-25 15:11:39 -0700

    Create contacts reducer

--- src/utilities/error_utility.js
+++ src/utilities/error_utility.js
@@ -33,7 +33,7 @@ export const defaultErrorAlert = (error) => {
   }
 
   amplitude.logEvent('Error - General', { error_description: error.description, error_message: error.message });
-  // console.error(error); // Debug Test
+  console.error(error); // Debug Test
 
   setTimeout(() => {
     Alert.alert('', alertString,
