1e9763490092d8aa65228b80e664b95e9044b6ba
Anshuman Dewangan <anshuman64@gmail.com>
2018-05-02 20:31:31 -0700

    Implement searchBar for createCircles

--- src/screens/create_circle_screen/create_circle_screen_container.js
+++ src/screens/create_circle_screen/create_circle_screen_container.js
@@ -6,8 +6,11 @@ import CreateCircleScreen from './create_circle_screen';
 
 //--------------------------------------------------------------------//
 
-const mapStateToProps = ({ conversations }, ownProps) => ({
+const mapStateToProps = ({ conversations, usersCache, groupsCache, contactsCache }, ownProps) => ({
   conversations:   conversations.conversations,
+  groupsCache:     groupsCache,
+  contactsCache:   contactsCache,
+  usersCache:      usersCache,
 });
 
 export default connect(
