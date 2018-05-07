a77aec77e01152c025d49d9d503c875c8134caa1
Anshuman Dewangan <anshuman64@gmail.com>
2018-05-07 16:55:53 -0700

    Implement DisplayNameScreen

--- src/screens/text_input_screen/text_input_screen_container.js
+++ src/screens/text_input_screen/text_input_screen_container.js
@@ -3,18 +3,20 @@ import { connect } from 'react-redux';
 
 // Local Imports
 import TextInputScreen                                from './text_input_screen';
-import { editUsername }                               from '../../actions/client_actions';
+import { editFullName, editUsername }                 from '../../actions/client_actions';
 import { createFriendRequest, sendFriendshipRequest } from '../../actions/friendship_actions';
 import { editGroupName }                              from '../../actions/group_actions';
 import { navigateTo, goBack }                         from '../../actions/navigation_actions';
 
 //--------------------------------------------------------------------//
 
-const mapStateToProps = ({ client }, ownProps) => ({
-  client: client,
+const mapStateToProps = ({ client, usersCache }, ownProps) => ({
+  client:     client,
+  usersCache: usersCache,
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
+  editFullName:          (authToken, firebaseUserObj, fullName)         => dispatch(editFullName(authToken, firebaseUserObj, fullName)),
   editUsername:          (authToken, firebaseUserObj, username)         => dispatch(editUsername(authToken, firebaseUserObj, username)),
   createFriendRequest:   (authToken, firebaseUserObj, userId, username) => dispatch(createFriendRequest(authToken, firebaseUserObj, userId, username)),
   sendFriendshipRequest: (sentFriendship)                               => dispatch(sendFriendshipRequest(sentFriendship)),
