6b797fbac822cf74a126a595807329c9afadd399
Anshuman Dewangan <anshuman64@gmail.com>
2018-05-07 21:42:41 -0700

    Fix maxWidth on postListItem

--- src/screens/text_input_screen/text_input_screen.js
+++ src/screens/text_input_screen/text_input_screen.js
@@ -237,13 +237,11 @@ class TextInputScreen extends React.PureComponent {
 
   _renderTextInput() {
     let placeholderText;
-    let maxLength = 12;
 
     if (this.props.screen === 'UsernameScreen' || this.props.screen === 'UsernameScreenLogin' || this.props.screen === 'AddFriendScreen') {
       placeholderText = 'Enter username';
     } else if (this.props.screen === 'DisplayNameScreen' || this.props.screen === 'DisplayNameScreenLogin') {
       placeholderText = 'Enter name';
-      maxLength = 20;
     } else if (this.props.screen === 'NameCircleScreen') {
       placeholderText = 'Enter circle name';
     } else if (this.props.screen === 'NameGroupScreen') {
@@ -259,7 +257,7 @@ class TextInputScreen extends React.PureComponent {
         placeholder={placeholderText}
         autoCapitalize={'none'}
         autoFocus={true}
-        maxLength={maxLength}
+        maxLength={18}
         returnKeyType={RN.Platform.OS === 'ios' ? 'done' : null}
         placeholderTextColor={COLORS.grey400}
         underlineColorAndroid={'transparent'}
