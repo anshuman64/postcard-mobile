eef933aee03b823d0f98b4b36e843d5e333b4f64
Anshuman Dewangan <anshuman64@gmail.com>
2018-05-08 17:44:02 -0700

    Fix duplicate messages bug

--- src/screens/messages_screen/messages_screen.js
+++ src/screens/messages_screen/messages_screen.js
@@ -346,7 +346,7 @@ class MessagesScreen extends React.PureComponent {
         ref={(ref) => this.flatList = ref}
         data={messages ? messages.data : null}
         renderItem={this._renderItem.bind(this)}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item, index) => String(index)}
         style={styles.messageList}
         initialNumToRender={10}
         maxToRenderPerBatch={25}
