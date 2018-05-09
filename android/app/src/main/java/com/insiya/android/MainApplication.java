3e9f2f127020b51603690013d86f620fbb0ba3d5
Anshuman Dewangan <anshuman64@gmail.com>
2018-05-09 08:17:08 -0700

    Install react-native-code-push

--- android/app/src/main/java/com/insiya/android/MainApplication.java
+++ android/app/src/main/java/com/insiya/android/MainApplication.java
@@ -3,6 +3,7 @@ package com.insiya.android;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
 import com.brentvatne.react.ReactVideoPackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
@@ -24,6 +25,12 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+
+        @Override
+        protected String getJSBundleFile() {
+        return CodePush.getJSBundleFile();
+        }
+    
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -33,6 +40,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
             new ReactVideoPackage(),
             new BridgePackage(),
             new ReactNativeContacts(),
