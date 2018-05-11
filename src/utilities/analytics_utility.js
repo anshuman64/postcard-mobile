db9717740e5cf8b6c104b457f32a9909b7d01b94
Anshuman Dewangan <anshuman64@gmail.com>
2018-05-11 13:55:54 -0700

    Add iOS camera permissions

--- src/utilities/analytics_utility.js
+++ src/utilities/analytics_utility.js
@@ -16,8 +16,6 @@ let setupAmplitude = () => {
 
   if (AMPLITUDE_ENV_SETTING === ENV_TYPES.PRODUCTION) {
     apiKey = '7ce84b314a6daad5ff9966ec1a2c52ab'; // key for insiya-production
-  } else if (AMPLITUDE_ENV_SETTING === ENV_TYPES.TEST) {
-    apiKey = '754c90a8ee32f23bd7042c47ea600e4d'; // key for insiya-test
   } else {
     apiKey = 'fa9aded0e5b7590482fffff78b2bd85c'; // key for insiya-dev
   }
