4d0dc003b6c0cec243626f88bbfac9495afe609f
Anshuman Dewangan <anshuman64@gmail.com>
2018-05-18 13:27:17 -0700

    Implement link preview item

---
+++ src/components/link_preview_item/link_preview_item.js
@@ -0,0 +1,143 @@
+// Library Imports
+import React       from 'react';
+import RN          from 'react-native';
+import ImageViewer from 'react-native-image-zoom-viewer';
+import VideoPlayer from 'react-native-video-player';
+
+// Local Imports
+import { styles }                      from './link_preview_item_styles';
+import { COLORS, getScaledHeight } from '../../utilities/style_utility';
+import { setStateCallback }            from '../../utilities/function_utility';
+
+//--------------------------------------------------------------------//
+
+/*
+Required Passed Props:
+  data (LinkPreview object): contains information about the link
+Optional Passed Props:
+  -
+*/
+class Medium extends React.PureComponent {
+
+  //--------------------------------------------------------------------//
+  // Constructor
+  //--------------------------------------------------------------------//
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isModalVisible: false,
+      isSpinnerVisible: true,
+      previewType:   null,
+      mediumDimensions: null,
+      scaledDimensions: null
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.data.contentType.startsWith('text/') && this.props.data.images) {
+      this._setMediumDimensions(this.props.data.images[0], 'text');
+    } else if (this.props.data.contentType.startsWith('image/')) {
+      this._setMediumDimensions(this.props.data.url, 'image');
+    } else if (this.props.data.contentType.startsWith('video/')) {
+      this._setMediumDimensions(this.props.data.url, 'video');
+    }
+  }
+
+  _setMediumDimensions = (url, previewType) => {
+    RN.Image.getSize(url, (width, height) => {
+      let scaledWidth = this.props.width;
+      let scaledHeight = getScaledHeight({ width: width, height: height }, scaledWidth);
+
+      this.setState({ previewType: previewType, mediumDimensions: { width: width, height: height }, scaledDimensions: { width: scaledWidth, height: scaledHeight } });
+    });
+  }
+
+  _onPressLink = () => {
+    RN.Linking.openURL(this.props.data.url);
+  }
+
+  //--------------------------------------------------------------------//
+  // Render Methods
+  //--------------------------------------------------------------------//
+
+  _renderVideo() {
+    return (
+      <RN.View style={[styles.mediumContainer, { width: this.state.scaledDimensions.width, height: this.state.scaledDimensions.height }]}>
+        <RN.ActivityIndicator size='small' color={COLORS.grey500} style={{position: 'absolute'}}/>
+        <VideoPlayer
+          style={{ width: this.state.scaledDimensions.width, height: this.state.scaledDimensions.height }}
+          video={{uri: this.props.data.url }}
+          videoWidth={this.state.mediumDimensions.width}
+          videoHeight={this.state.mediumDimensions.height}
+          autoPlay={true}
+          defaultMuted={true}
+          disableControlsAutoHide={true}
+          disableFullscreen={false}
+          pauseOnPress={true}
+          fullScreenOnLongPress={true}
+          />
+      </RN.View>
+    )
+  }
+
+  _renderImage() {
+    let url = this.state.previewType === 'text' ? this.props.data.images[0] : this.props.data.url;
+
+    return (
+      <RN.View style={[styles.mediumContainer, { width: this.state.scaledDimensions.width, height: this.state.scaledDimensions.height }]}>
+        {this.state.isSpinnerVisible ?
+          <RN.ActivityIndicator size='small' color={COLORS.grey500} style={{position: 'absolute'}}/> :
+        null}
+        <RN.Image
+          source={{uri: url }}
+          style={{ width: this.state.scaledDimensions.width, height: this.state.scaledDimensions.height }}
+          resizeMode={'cover'}
+          onLoad={setStateCallback(this, { isSpinnerVisible: false })}
+          />
+      </RN.View>
+    )
+  }
+
+  _renderMedium() {
+    if (this.state.previewType === 'text' || this.state.previewType === 'image') {
+      return this._renderImage();
+    } else if (this.state.previewType === 'video') {
+      return this._renderVideo();
+    } else {
+      return null;
+    }
+  }
+
+  _renderCaption() {
+    return (
+      <RN.View style={styles.captionView}>
+        {this.props.data.title ?
+          <RN.Text allowFontScaling={false} numberOfLines={2} style={styles.titleText}>
+            {this.props.data.title}
+          </RN.Text> :
+          null
+        }
+        <RN.Text allowFontScaling={false} style={styles.urlText}>
+          {'www.youtube.com'}
+        </RN.Text>
+      </RN.View>
+    )
+  }
+
+  render() {
+    return (
+    <RN.TouchableWithoutFeedback onPress={this._onPressLink}>
+        <RN.View style={styles.container}>
+          {this._renderMedium()}
+          {this._renderCaption()}
+        </RN.View>
+    </RN.TouchableWithoutFeedback>
+    )
+  }
+}
+
+//--------------------------------------------------------------------//
+
+export default Medium;
