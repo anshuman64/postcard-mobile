e98369f33b98bb0b9f395bf4e7c6837aa3b3b1a9
Anshuman Dewangan <anshuman64@gmail.com>
2018-05-18 14:25:29 -0700

    Link react-native-link-preview

--- android/app/src/main/java/com/insiya/android/MainApplication.java
+++ android/app/src/main/java/com/insiya/android/MainApplication.java
@@ -3,6 +3,7 @@ package com.insiya.android;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.linkpreview.RNReactNativeLinkPreviewPackage;
 import com.avishayil.rnrestart.ReactNativeRestartPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.brentvatne.react.ReactVideoPackage;
@@ -40,6 +41,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNReactNativeLinkPreviewPackage(),
             new ReactNativeRestartPackage(),
             new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
             new ReactVideoPackage(),
